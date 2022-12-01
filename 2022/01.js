0eec1dc81bc7b3f60d0f3e46e63f45e4cda1a4a0
kate <kate@dariox.club>
2022-12-01 13:46:37 +0800

    Add day-one for 2022

---
+++ 2022/01.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+let content = fs.readFileSync('01.txt').toString()
+
+let splitted = content.split('\n')
+let chunked = []
+let working = []
+for (let i = 0; i < splitted.length; i++) {
+    let item = splitted[i]
+    if (item.length < 1) {
+        chunked.push(working)
+        working = []
+    } else {
+        working.push(item)
+    }
+}
+
+let countmap = []
+let highestCountIndex = -1;
+let highestCount = 0;
+for (let i = 0; i < chunked.length; i++) {
+    let numchunk = chunked[i].map(v => parseInt(v))
+    let sum = numchunk.reduce((a, b) => a + b)
+    if (sum > highestCount) {
+        highestCountIndex = i;
+        highestCount = sum
+    }
+    countmap.push({
+        count: sum,
+        index: i
+    })
+}
+console.log(`Highest count index: ${highestCountIndex}`)
+console.log(`Count: ${highestCount}`)
+
+let countmapSorted = countmap.sort((a,b) => b.count - a.count)
+let topthree = 0
+for (let i = 0; i < 3; i++) {
+    console.log(`${i+1}: ${countmapSorted[i].count}`)
+    topthree += countmapSorted[i].count
+}
+console.log(`Top 3 has ${topthree} calories`)
