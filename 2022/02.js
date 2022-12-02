247fb982e592a6e2887ed23d8dc5aec59774e6f6
kate <kate@dariox.club>
2022-12-02 13:44:34 +0800

    Day 2 pt2 Solution

--- 2022/02.js
+++ 2022/02.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
 const os = require('os')
 
+/**
+ * loose:    0
+ * draw:     3
+ * win:      6
+ * 
+ * rock:     1
+ * paper:    2
+ * scissors: 3
+ */
+
 let scoretable = {
     'A X': 1 + 3,
     'A Y': 2 + 6,
@@ -13,6 +23,18 @@ let scoretable = {
     'C Z': 3 + 3
 }
 
+let scoretable_second = {
+    'A X': 3,
+    'A Y': 1 + 3,
+    'A Z': 2 + 6,
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+    'C X': 2 + 0,
+    'C Y': 3 + 3,
+    'C Z': 1 + 6
+}
+
 let data = fs.readFileSync('02.txt')
     .toString()
     .split(os.EOL)
@@ -23,3 +45,9 @@ for (let item of data) {
     total += scoretable[item]
 }
 console.log(`Total: ${total}`)
+
+let total_second = 0
+for (let item of data) {
+    total_second += scoretable_second[item]
+}
+console.log(`Total: ${total_second} (part 2)`)
