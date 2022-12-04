7de299a2d673648b7f4b236eb59dac63bb32b29f
kate <kate@dariox.club>
2022-12-04 13:42:44 +0800

    d4p1

--- 2022/04.js
+++ 2022/04.js
@@ -1,21 +1,61 @@
 const fs = require('fs')
 const { EOL } = require('os')
 
-const lines = fs.readFileSync('04-sample.txt')
+const lines = fs.readFileSync('04.txt')
     .toString()
     .split(EOL)
 
 function calculateGroups() {
     let tempgroup = []
-    let groups = []
+    let ret = []
     for (let i of lines) {
-        if (i.length < 1) {
-            groups.push(tempgroup)
-        } else {
-            tempgroup.push(i)
-        }
+        ret.push(i.split(','))
     }
-    return groups
+    return ret
 }
 
 const groups = calculateGroups()
+
+function parseGroup(group) {
+    let items = []
+    for (let line of group) {
+        let c = line.split('-')
+        items.push([
+            parseInt(c[0]),
+            parseInt(c[1])
+        ])
+    }
+    return items
+}
+
+let globalCount = 0
+let groupTable = []
+for (let groupLineIndex in groups) {
+    let groupLine = groups[groupLineIndex]
+    let group = parseGroup(groupLine)
+    for (let innerIndex in group) {
+        let inner = group[innerIndex]
+        let bk = false
+        for (let outerIndex in group) {
+            let outer = group[outerIndex]
+            if (outerIndex != innerIndex &&
+                inner[0] >= outer[0] &&
+                inner[0] <= outer[1] &&
+                inner[1] <= outer[1] &&
+                inner[1] >= outer[0]) {
+                    groupTable.push({
+                        groupLineIndex,
+                        innerIndex,
+                        outerIndex
+                    })
+                globalCount++;
+                bk = true;
+                break;
+            }
+        }
+        if (bk)
+            break;
+    }
+}
+console.table(groupTable)
+console.log(`total: ${globalCount}`)
