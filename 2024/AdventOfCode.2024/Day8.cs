2c14c3f6b849d92024034d05cc52616b620e1ae7
kate <kate@dariox.club>
2024-12-08 11:45:24 +0800

    Prepare for Day 8

---
+++ 2024/AdventOfCode.2024/Day8.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+
+namespace AdventOfCode.TwentyTwentyFour;
+
+[DefaultValue(8)]
+public class DayEight : IDayHandler
+{
+    public void Run(string[] input)
+    {
+        
+    }
+}
