29e03e00377abca556b4f16c49f154a6b7abe77c
kate <kate@dariox.club>
2024-12-10 12:24:05 +0800

    Prepare for Day 10

---
+++ 2024/AdventOfCode.2024/Day10.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel;
+
+namespace AdventOfCode.TwentyTwentyFour;
+
+[DefaultValue(10)]
+public class Day10 : IDayHandler
+{
+    public void Run(string[] lines)
+    {
+    }
+}
