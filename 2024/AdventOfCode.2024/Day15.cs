13e0d2cd712bc363c659ce8d2c17c6ae31afb1db
kate <kate@dariox.club>
2024-12-15 12:30:31 +0800

    Prepare for Day 15

---
+++ 2024/AdventOfCode.2024/Day15.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+
+namespace AdventOfCode.TwentyTwentyFour;
+
+[DefaultValue(15)]
+public class Day15 : IDayHandler
+{
+    public void Run(string[] inputData)
+    {
+        var a = PartOne(inputData);
+        Console.WriteLine($"Part One: {a}");
+        var b = PartTwo(inputData);
+        Console.WriteLine($"Part Two: {b}");
+    }
+
+    public long PartOne(string[] inputData)
+    {
+        long result = 0;
+        return result;
+    }
+    public long PartTwo(string[] inputData)
+    {
+        long result = 0;
+        return result;
+    }
+}
