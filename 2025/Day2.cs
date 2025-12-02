5eb4c88fb2099ab1064954a7faee0373b544e854
kate <kate@dariox.club>
2025-12-02 20:16:47 +0800

    [2025, Day 2] Remove redundant code for more performance optimizations

--- 2025/Day2.cs
+++ 2025/Day2.cs
@@ -7,13 +7,8 @@ public class Day2 : IDayHandler
 {
     public void Run(string[] contentL)
     {
-        var asdf = string.Join("", contentL.Select(e => e.Trim()))
+        var content = string.Join("", contentL)
             .Split(',')
-            .Where(e => !string.IsNullOrEmpty(e))
-            .ToArray();
-
-        foreach (var x in asdf) Console.WriteLine(x);
-        var content = asdf
             .Select(e =>
             {
                 var s = e.Split('-');
@@ -28,12 +23,11 @@ public class Day2 : IDayHandler
         long ptB = 0;
         for (int i = 0; i < content.Length; i++)
         {
-            var reps = new List<string>();
             for (long j = content[i][0]; j <= content[i][1]; j++)
             {
                 var js = j.ToString();
-                var half = Convert.ToInt32(Math.Floor(js.Length / 2f));
-                if (js.Substring(half) == js.Substring(0, half))
+                var half = js.Length / 2;
+                if (js[half..] == js[..half])
                 {
                     ptA += j;
                     // Console.WriteLine($"A: ({j}) {content[i][0]}-{content[i][1]}");
