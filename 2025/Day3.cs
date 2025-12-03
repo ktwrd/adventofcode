84789766e49d1098dbcf9a46c7aa687f4cc740da
kate <kate@dariox.club>
2025-12-03 14:33:58 +0800

    2025: Day 3

--- 2025/Day3.cs
+++ 2025/Day3.cs
@@ -3,7 +3,31 @@ namespace AdventOfCode.TwentyTwentyFive;
 [Advent(2025, 3)]
 public class Day3 : IDayHandler
 {
-    public void Run(string[] contentL)
+    public void Run(string[] content)
     {
+        var part1 = Calc(ref content, 2);
+        var part2 = Calc(ref content, 12);
+        Console.WriteLine($"Part One: {part1}");
+        Console.WriteLine($"Part Two: {part2}");
+    }
+    private static long Calc(ref string[] content, int k)
+    {
+        long v = 0;
+        foreach (var line in content)
+        {
+            var s = new List<char>();
+            var r = line.Length - k;
+            foreach (var c in line)
+            {
+                while (r > 0 && s.Count > 0 && s[^1] < c)
+                {
+                    s.RemoveAt(s.Count - 1);
+                    r--;
+                }
+                s.Add(c);
+            }
+            v += long.Parse(new string(s.Take(k).ToArray()));
+        }
+        return v;
     }
 }
