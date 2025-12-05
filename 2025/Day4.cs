4abb59d50a065c9f8dfd8b2044347f7d6f89ccd4
kate <kate@dariox.club>
2025-12-05 21:36:50 +0800

    2025 Day 4: Reduced memory usage to ~46.5KiB (slightly slower by ~1-2ms for non AOT)

--- 2025/Day4.cs
+++ 2025/Day4.cs
@@ -9,15 +9,19 @@ public class Day4 : IDayHandler
         short partOne = 0;
         short partTwo = 0;
         var anyDeleted = true;
-        var toDot = new byte[content[0].Length,content.Length];
+        var width = content[0].Length;
+        var height = content.Length;
+        var toDot = new byte[(width/8) + 1,height];
         var doingPartOne = true;
         while (anyDeleted)
         {
             short ac = 0;
-            for (int x = 0; x < content[0].Length; x++)
+            for (int y = 0; y < height; y++)
             {
-                for (int y = 0; y < content.Length; y++)
+                for (int x = 0; x < width; x++)
                 {
+                    var xa = x / 8;
+                    var xb = (byte)(1 << (x%8));
                     if (content[y][x] == at)
                     {
                         byte rollCount = 0;
@@ -27,18 +31,17 @@ public class Day4 : IDayHandler
                             int im = i % 3;
                             var ry = i < 3 ? -1 : i < 6 ? 0 : 1;
                             var py = ry + y;
-                            if (py < 0 || py >= content.Length) continue;
+                            if (py < 0 || py >= height) continue;
 
                             var rx = im == 0 ? -1 : im == 2 ? 1 : 0;
                             var px = rx + x;
-                            if (px < 0 || px >= content[0].Length) continue;
+                            if (px < 0 || px >= width) continue;
 
                             if (content[py][px] == at) rollCount++;
                         }
                         if (rollCount < 4)
                         {
-                            if (toDot[x,y] == 0) ac++;
-                            toDot[x,y] = 1;
+                            toDot[xa,y] |= xb;
                             if (doingPartOne)
                             {
                                 partOne++;
@@ -47,12 +50,16 @@ public class Day4 : IDayHandler
                     }
                 }
             }
-            for (int x = 0; x < content[0].Length; x++)
+            for (int y = 0; y < height; y++)
             {
-                for (int y = 0; y < content.Length; y++)
+                for (int x = 0; x < width; x++)
                 {
-                    if (toDot[x,y] != 0) content[y][x] = dot;
-                    toDot[x,y] = 0;
+                    var xa = x / 8;
+                    var xb = (byte)(1 << (x%8));
+                    if ((toDot[xa,y] & xb) != xb) continue;
+                    content[y][x] = dot;
+                    toDot[xa,y] = 0;
+                    ac++;
                 }
             }
             anyDeleted = ac > 0;
