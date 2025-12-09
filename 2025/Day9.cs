dceb42a6664527e7da47efe46a1eb5cedbabf244
kate <kate@dariox.club>
2025-12-09 12:15:00 +0800

    Prep for Day 9

---
+++ 2025/Day9.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.TwentyTwentyFive;
+
+[Advent(2025, 9)]
+public class Day9 : IDayHandler
+{
+    public void Run(string[] content, out object partOne, out object partTwo)
+    {
+        partOne = 0;
+        partTwo = 0;
+    }
+}
