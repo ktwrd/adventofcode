2333b869dc162115a711720e7f892f4827c14c0b
kate <kate@dariox.club>
2025-12-10 16:14:35 +0800

    Updated Extensions.cs

--- AdventOfCode.Shared/Extensions.cs
+++ AdventOfCode.Shared/Extensions.cs
@@ -48,4 +48,36 @@ public static class Extensions
 
     public static bool PolygonIntersects(this IEnumerable<QEdge> polygon, QEdge edge) => polygon.Any(e => e.Intersects(edge));
     public static bool PolygonIntersects(this IEnumerable<QDEdge> polygon, QDEdge edge) => polygon.Any(e => e.Intersects(edge));
+
+    public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+{
+    if (sequence == null)
+    {
+        yield break;
+    }
+
+    var list = sequence.ToList();
+
+    if (!list.Any())
+    {
+        yield return Enumerable.Empty<T>();
+    }
+    else
+    {
+        var startingElementIndex = 0;
+
+        foreach (var startingElement in list)
+        {
+            var index = startingElementIndex;
+            var remainingItems = list.Where((e, i) => i != index);
+
+            foreach (var permutationOfRemainder in remainingItems.Permute())
+            {
+                yield return permutationOfRemainder.Prepend(startingElement);
+            }
+
+            startingElementIndex++;
+        }
+    }
+}
 }
